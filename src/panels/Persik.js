1030037759774c214adab2bec56c4bf50e77efd3
Alexander Balandin <al.balandin@corp.vk.com>
2022-10-14 10:12:49 +0300

    add local video

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -18,7 +18,8 @@ const Persik = props => {
       <Group>
         <Player
           videoId={0}
-          src="https://media.lovina.vkpartner.ru/s0/story/8bb0d9011990a72f3ee0963e015878a6ccceee0200/large.mp4"
+          // src={"https://media.lovina.vkpartner.ru/s0/story/8bb0d9011990a72f3ee0963e015878a6ccceee0200/large.mp4"}
+          src={process.env.PUBLIC_URL + '/video.mp4'}
           // src="https://vd344.mycdn.me/?expires=1662117356009&srcIp=85.209.1.9&pr=63&srcAg=CHROME_MAC&ms=185.226.53.60&type=3&sig=VmPqJnV867A&ct=0&urls=185.226.52.44&clientType=32&id=2763115989697"
         />
       </Group>
